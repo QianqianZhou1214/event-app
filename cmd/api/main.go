7b3ec7fc9188a11356e05d2e5bc995ff5cce6f78
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-19 13:19:50 +0200

    api setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"database/sql"
+	"event-app/internal/database"
+	"event-app/internal/env"
 	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+
+	_ "github.com/joho/godotenv/autoload"
 )
+
+type application struct {
+	port      int
+	jwtSecret string
+	models    database.Models
+}
 
 func main() {
 	db, err := sql.Open("sqlite3", "./data.db")
@@ -11,4 +23,15 @@ func main() {
 		log.Fatal(err)
 	}
 	defer db.Close()
+
+	models := database.NewModels(db)
+	app := &application{
+		port:      env.GetEnvInt("PORT", 8080),
+		jwtSecret: env.GetEnvString("JWT_SECRET", "some-secret-123456"),
+		models:    models,
+	}
+
+	if err := app.serve(); err != nil {
+		log.Fatal(err)
+	}
 }
